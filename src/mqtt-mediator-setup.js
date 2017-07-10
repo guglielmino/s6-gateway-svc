69053cdf41417978f0efd2c27073d142bd9de912
Fabrizio Guglielmino <guglielmino@gmail.com>
2017-07-10 19:40:38 +0200

    Fix power command feedback handler

--- src/mqtt-mediator-setup.js
+++ src/mqtt-mediator-setup.js
@@ -4,6 +4,7 @@ import EventsMediator from './networks/devices/events-mediator';
 import StatResultHandler from './networks/devices/handlers/stat-result-handler';
 import LwtHandler from './networks/devices/handlers/lwt-handler';
 import TelemetryHandler from './networks/devices/handlers/telemetry-handler';
+import ResultHandler from './networks/devices/handlers/result-handler';
 
 
 import RestClient from './networks/http/rest-client';
@@ -13,6 +14,7 @@ const MediatorSetup = () => {
 
   const httpPublisher = payload => RestClient().post(`${config.api.url}events/`, payload);
 
+  mqttMediator.addHandler(/^tele\/.*\/RESULT$/, ResultHandler(httpPublisher));
   mqttMediator.addHandler(/^stat\/.*\/RESULT$/, StatResultHandler(httpPublisher));
   mqttMediator.addHandler(/^tele\/.*\/LWT$/, LwtHandler(httpPublisher));
   mqttMediator.addHandler(/^tele\/.*\/TELEMETRY$/, TelemetryHandler(httpPublisher));
